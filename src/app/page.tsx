1840d61ac6bd3a75e8975ec4d693f640306ad32f
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-08-31 08:06:57 +0530

    Add clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
       <section className="m-2">
         <p>Discord</p>
-        <Button variant={"default"}>Sign In</Button>
+        <UserButton afterSignOutUrl="/" />
       </section>
     </main>
   );
